4db43925ee5392c1fb1d95cffe72c8da86240f68
Ivan <ivanmatveev1001@gmail.com>
2022-08-07 17:37:31 +0300

    домашнее задание 1

---
+++ HW№1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное.
+Console.WriteLine("Введите первое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n > m)
+{
+    Console.WriteLine(n);
+}
+
+else
+{
+   Console.WriteLine(m); 
+}
