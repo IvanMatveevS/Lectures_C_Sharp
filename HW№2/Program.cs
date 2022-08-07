452fd0e5bf44de1124ca7a3f859395473aa55e9a
Ivan <ivanmatveev1001@gmail.com>
2022-08-08 01:13:41 +0300

    задание2

---
+++ HW№2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое целое число");
+int k = Convert.ToInt32(Console.ReadLine());
+if(n > m)
+{
+    if(n > k)
+    {
+        Console.WriteLine(n);
+    }
+    else
+    {
+        Console.WriteLine(k);
+    }
+}
+else 
+{
+    if(m > k)
+    {
+       Console.WriteLine(m); 
+    }
+    else
+    {
+        Console.WriteLine(k);
+    }
+}
