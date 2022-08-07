1f9eb2119ce21030308a5b555c752dbcbc6ec983
Ivan <ivanmatveev1001@gmail.com>
2022-08-07 17:28:00 +0300

    семинар 1

---
+++ Sammer001/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n == m * m)
+{
+    Console.WriteLine("да");
+}
+
+else
+{
+   Console.WriteLine("нет"); 
+}
+
+
+// if(result.ToLower() == m)
+// {
+//     Console.Write("Ура подходит!");
+// }
+// else
+// {
+//     Console.Write("К сожалению нет ");
+//     Console.WriteLine(result);
+// }
