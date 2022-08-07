a60c1fa5d1f9ca95fe2a0a069ce43e9b12678bea
Ivan <ivanmatveev1001@gmail.com>
2022-08-07 17:29:08 +0300

    семинар 2

---
+++ Sammer002/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите целое число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+switch (n)
+{
+case 1:
+{
+    Console.WriteLine("Понед");
+    break;
+}
+case 2:
+{
+    Console.WriteLine("Вт");
+    break;
+}
+
+case 3:
+{
+    Console.WriteLine("ср");
+   break;
+}
+case 4:
+{
+    Console.WriteLine("Чт");
+    break;
+}
+case 5:
+{
+    Console.WriteLine("Пят");
+    break;
+}
+case 6:
+{
+    Console.WriteLine("Суб");
+    break;
+}
+case 7:
+{
+    Console.WriteLine("Вс");
+    break;
+}
+
+}
