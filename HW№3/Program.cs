d0112d0ace8658928f304c9bd475af7e78a98d0d
Ivan <ivanmatveev1001@gmail.com>
2022-08-08 09:15:29 +0300

    задание3

---
+++ HW№3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if(0 == n % 2)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
